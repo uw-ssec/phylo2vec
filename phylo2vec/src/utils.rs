c1637c0baa00adbb42137b42753d876f8d33f765
ayushnag <35325113+ayushnag@users.noreply.github.com>
2024-10-22 20:16:22 -0700

    fix: update sample ranges

--- phylo2vec/src/utils.rs
+++ phylo2vec/src/utils.rs
@@ -4,8 +4,8 @@ pub fn sample_ordered(n_leaves: usize) -> Vec<usize> {
     let mut v: Vec<usize> = Vec::with_capacity(n_leaves);
     let mut rng = rand::thread_rng();
 
-    for i in 0..n_leaves {
-        v.push(rng.gen_range(0..=i));
+    for i in 0..(n_leaves - 1) {
+        v.push(rng.gen_range(0..(i + 1)));
     }
     v
 }
@@ -14,8 +14,8 @@ pub fn sample_unordered(n_leaves: usize) -> Vec<usize> {
     let mut v: Vec<usize> = Vec::with_capacity(n_leaves);
     let mut rng = rand::thread_rng();
 
-    for i in 0..n_leaves {
-        v.push(rng.gen_range(0..=2 * i));
+    for i in 0..(n_leaves - 1) {
+        v.push(rng.gen_range(0..(2 * i + 1)));
     }
     v
 }
