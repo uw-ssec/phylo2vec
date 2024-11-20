123b9700703c5e37417a530617964cdcd35bb737
ayushnag <35325113+ayushnag@users.noreply.github.com>
2024-11-20 11:59:43 -0800

    chore: setup ancestry_dtype comparison

--- phylo2vec/benches/bench_main.rs
+++ phylo2vec/benches/bench_main.rs
@@ -2,4 +2,5 @@ use criterion::criterion_main;
 
 mod benchmarks;
 
-criterion_main!(benchmarks::get_pairs::get_pairs);
+// criterion_main!(benchmarks::get_pairs::get_pairs);
+criterion_main!(benchmarks::get_ancestry_dtype::get_ancestry_datatypes);
