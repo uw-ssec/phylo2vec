ec53c1af8ef79a4ec63169867ebc0a25f521b9f7
Landung 'Don' Setiawan <landungs@uw.edu>
2024-12-19 09:26:34 -0800

    ci: Add six dependency for ete3 and move tests

--- py-phylo2vec/tests/test_utils.py
+++ py-phylo2vec/tests/test_utils.py
@@ -7,7 +7,7 @@ import pytest
 
 from ete3 import Tree
 
-from phylo2vec.tests.config import MIN_N_LEAVES, MAX_N_LEAVES, N_REPEATS
+from .config import MIN_N_LEAVES, MAX_N_LEAVES, N_REPEATS
 from phylo2vec.base import to_newick
 from phylo2vec.utils import (
     add_leaf,
