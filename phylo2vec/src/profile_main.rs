3bc792df25c9c2998b8e1fcde532118fb3d92ce0
ayushnag <35325113+ayushnag@users.noreply.github.com>
2025-04-15 17:42:01 -0700

    chore: add rust profiling to workflow

--- phylo2vec/src/profile_main.rs
+++ phylo2vec/src/profile_main.rs
@@ -7,21 +7,14 @@ use std::env;
 const DEFAULT_N_LEAVES: usize = 100000;
 
 fn main() {
-    // This is a placeholder for the main function.
-    // You can add your code here to implement the functionality you need.
     let args: Vec<String> = env::args().collect();
-
-    // Default to 5 if no argument is provided
     let n_leaves = if args.len() > 1 {
         args[1].parse::<usize>().unwrap_or(DEFAULT_N_LEAVES)
     } else {
         DEFAULT_N_LEAVES
     };
-    // Add your logic here
-    // For example, you might want to load data, process it, and output results.
-    // ...
     let v = sample_vector(n_leaves, true);
     let n = to_newick(&v);
     let re_v = to_vector(&n);
-    println!("v len: {:?}", re_v.len());
+    println!("vector length: {:?}", re_v.len());
 }
